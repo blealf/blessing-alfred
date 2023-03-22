e61dd813af411cb3cfde3b5843f56e180cbb7ec9
BLESSING ALFRED <blessing.alfred@mypatricia.co>
2023-03-22 15:17:23 +0100

    work, remains content

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 import Heading from '../Heading'
 import './Work.scss'
+import github from '../../assets/nav/github.svg'
+import link from '../../assets/link.svg'
 
 const Work = () => {
+  const tags = 'VS Code Sublime Text Atom iTerm2 Hyper'.split(' ')
+  const willFloatRight = (value) => {
+    return (value + 1) % 2 === 0
+  }
+  const work = [1,2,3]
   return (
     <div className="work">
       <Heading title="Work." />
-      
+        {work.map((item, index) => {
+          return (
+            <div className="work_wrapper">
+              <div className="image" style={{
+                marginLeft: willFloatRight(index) ? '' : 'auto'
+              }}></div>
+              <div className="float" style={{
+                alignItems: willFloatRight(index) ? 'flex-end' : 'flex-start',
+                right: willFloatRight(index) ? 0 : '',
+              }}>
+                <p>Project</p>
+                <h3>MY PROJECT</h3>
+                <div className="description">
+                  A minimal, dark blue theme for VS Code, Sublime Text, Atom, iTerm, and more. Available on Visual Studio Marketplace, Package Control, Atom Package Manager, and npm.
+                </div>
+                <div className="tags">
+                  {tags.map(tag => <span>{tag}</span>)}
+                </div>
+                <div className="icons">
+                  <a href="https://github.com" target="_blank">
+                    <img src={github} alt="github" />
+                  </a>
+                  <a href="https://github.com" target="_blank">
+                    <img src={link} alt="link" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          )
+        })}
     </div>
   )
 }
